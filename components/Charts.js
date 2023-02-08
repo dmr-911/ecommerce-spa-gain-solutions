52f17a1e8fe600954e23dd2ab43dee68d658b65c
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-08 17:08:08 +0600

    charts updated

--- components/Charts.js
+++ components/Charts.js
@@ -1,21 +1,6 @@
 import React from "react";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  PieChart,
-  Pie,
-  Sector,
-  Cell,
-} from "recharts";
-
-import { Chart } from "react-google-charts";
 import { useFilterContext } from "@/context/FilterContextProvider";
+import { Chart } from "react-google-charts";
 
 const Charts = () => {
   const { all_products } = useFilterContext();
@@ -42,39 +27,40 @@ const Charts = () => {
       ).length,
     ],
   ];
+  const optionsPie = {
+    colors: ["#0095A0", "#84AF27", "#FFC239"],
+  };
 
   // data bar chart
-  const data = [
-    {
-      name: "Official",
-      uv: 4000,
-      pv: all_products?.filter((p) => p.official_warranty).length,
-      amt: 2400,
-    },
-    {
-      name: "Unofficial",
-      uv: 3000,
-      pv: all_products?.filter((p) => p.unofficial_warranty).length,
-      amt: 2210,
-    },
-    {
-      name: "Without Warranty",
-      uv: 2000,
-      pv: all_products?.filter((p) => p.no_warranty).length,
-      amt: 2290,
-    },
-    {
-      name: "Used",
-      uv: 2780,
-      pv: all_products?.filter((p) => p.used_phone).length,
-      amt: 2000,
-    },
+  const dataBar = [
+    ["", "", "", ""],
+    ["Official", 0, all_products?.filter((p) => p.official_warranty).length, 0],
+    [
+      "Unofficial",
+      0,
+      all_products?.filter((p) => p.unofficial_warranty).length,
+
+      0,
+    ],
+    [
+      "Without Warranty",
+      0,
+      all_products?.filter((p) => p.no_warranty).length,
+
+      0,
+    ],
+    ["Used", 0, all_products?.filter((p) => p.used_phone).length, 0],
   ];
+
+  const optionsBar = {
+    colors: ["#0095A0"],
+  };
+
   return (
-    <div className="default-margin mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className="default-margin mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 mb-12">
       {/* Pie chart */}
       <div>
-        <p className="text-[#575757] text-2xl font-bold leading-9 my-9">
+        <p className="text-[#575757] text-2xl font-bold leading-9 my-4 md:my-9">
           Sources
         </p>
         <Chart
@@ -82,35 +68,21 @@ const Charts = () => {
           data={dataPie}
           width={"100%"}
           height={"400px"}
+          options={optionsPie}
         />
       </div>
       {/* Bar chart */}
       <div>
-        <p className="text-[#575757] text-2xl font-bold leading-9 my-9">
+        <p className="text-[#575757] text-2xl font-bold leading-9 my-4 md:my-9">
           Conditions
         </p>
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-          barSize={20}
-        >
-          <XAxis
-            dataKey="name"
-            scale="point"
-            padding={{ left: 40, right: 40 }}
-          />
-          <YAxis />
-          <Tooltip />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Bar dataKey="pv" fill="#0095A0" background={{ fill: "#eee" }} />
-        </BarChart>
+        <Chart
+          chartType="Bar"
+          width="100%"
+          height="400px"
+          data={dataBar}
+          options={optionsBar}
+        />
       </div>
     </div>
   );
